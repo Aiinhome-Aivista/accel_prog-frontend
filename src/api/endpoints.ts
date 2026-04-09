7ffbd1b21c637791463288713a973cc297582cb5
Arpan <duttaarpan3102@gmail.com>
2026-04-09 09:53:13 +0530

    fix

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,7 +3,7 @@ export const API_ENDPOINTS = {
   REGISTER: "/auth/register",
   USERS: "/users",
   COURSE: "/api/v1/courses",
-  REGISTRATION: "api/v1/register",
+  REGISTRATION: "/api/v1/register",
   SEND_OTP: "/api/v1/send-otp",
   VERIFY_OTP: "/api/v1/verify-otp",
   GOOGLE_SIGNIN: "/api/v1/google-signin",
