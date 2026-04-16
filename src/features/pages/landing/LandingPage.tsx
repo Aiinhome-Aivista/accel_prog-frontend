c855270205c343a43d9b216ac03c9c361db616a0
Soma Kundu <somakund09@gmail.com>
2026-04-16 12:36:32 +0530

    UI Circle increase size

--- src/features/pages/landing/LandingPage.tsx
+++ src/features/pages/landing/LandingPage.tsx
@@ -587,7 +587,7 @@ function LandingPage({
           <div className="cta-box fade-in">
             <img
               src={HeroIcon}
-              className="mb-6 h-60 w-60"
+              className="mb-0 h-60 w-60"
               alt="Logo"
               aria-hidden="true"
             />
